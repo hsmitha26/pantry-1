e289f8dad256d1808ed0ac62ae3b3607033031ba
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-26 11:03:45 -0700

    started on summary method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -9,4 +9,14 @@ class CookBook
   def add_recipe(recipe_object)
     @recipes << recipe_object
   end
+
+  def summary
+    #need to group @recipes array to match interaction pattern.
+    #lots of nesting.
+    #all info needed is in @recipes array. total_calories is already in recipe.rb
+    x = @recipes.group_by do |recipe_object|
+      recipe_object.name
+    end
+    binding.pry
+  end
 end
